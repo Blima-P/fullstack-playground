8040130c5d2f7c3724d58ae02a21063082ae4763
Pedro Braga de Lima <p1braga.tech@gmail.com>
2025-09-18 11:16:30 -0300

    Nova home demo

---
+++ Front-end/welcome/actions.js
@@ -0,0 +1,4 @@
+// Botão para redirecionar à próxima página
+document.getElementById("goBtn").addEventListener("click", () => {
+  window.location.href = "painel.html"; // Troque para a página real que você quer abrir
+});
