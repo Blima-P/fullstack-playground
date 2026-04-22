c834faa215b02b9ebf952a63fb815d721cd5c93e
Pedro Braga de Lima <pbl0812@gmail.com>
2026-04-22 11:29:37 -0300

    restart

--- Backend/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.exemplo.cadastro.repository;
-
-import com.exemplo.cadastro.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-}
